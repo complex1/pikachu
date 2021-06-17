0a708e2fe251cd84f980dbc205995b21db21baac
complex1 <mauryas367@gmail.com>
2021-06-17 15:06:29 +0530

    added build script

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,7 @@
 const path = require("path")
 module.exports = {
   configureWebpack: {
+    entry: path.resolve(__dirname, 'client/main.js'),
     resolve: {
       alias: {
         '@': path.resolve(__dirname, 'client'),
